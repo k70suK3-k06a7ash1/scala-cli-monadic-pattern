0cbc0115592219af7c5d566fd4d7ad93d7ad5d66
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-04-14 05:48:22 +0900

    chore

--- src/shared/Shared.scala
+++ src/shared/Shared.scala
@@ -1,18 +1,18 @@
 package shared // 共通定義用のパッケージ
 
-// --- 1. Monad トレイトの定義 ---
+// --- Monad トレイトの定義 ---
 trait Monad[M[_]] {
   def pure[A](value: A): M[A]
   def flatMap[A, B](ma: M[A])(f: A => M[B]): M[B]
   def map[A, B](ma: M[A])(f: A => B): M[B] = flatMap(ma)(a => pure(f(a)))
 }
 
-// --- 2. Maybe データ型の定義 ---
+// --- Maybe データ型の定義 ---
 sealed trait Maybe[+A]
 case class Just[+A](value: A) extends Maybe[A]
 case object Nothing extends Maybe[Nothing]
 
-// --- 3. Maybe 型に for式のためのメソッドを追加する implicit class ---
+// --- Maybe 型に for式のためのメソッドを追加する implicit class ---
 // これも共通定義として利用可能にする
 implicit class MaybeOps[+A](ma: Maybe[A]) {
   /** flatMap を Maybe 自身に実装 */
